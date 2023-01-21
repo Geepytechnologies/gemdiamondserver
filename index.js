222802af2be8b3b90f78055f20bb4afa9a7bf4b2
geepy technologies <geepytechnologies@gmail.com>
2023-01-21 11:24:47 +0100

    new

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const userRoute = require('./routes/users')
 dotenv.config();
 
 app.use(express.json());
-app.use(cors({origin: "https://gemukdiamond.com"}));
+app.use(cors({origin: "https://gemukdiamond.com",credentials: true}));
 app.use((req,res,next)=>{
   res.header({"Access-Control-Allow-Credentials": "true","Access-Control-Allow-Origin": "https://gemukdiamond.com" })
   next();
