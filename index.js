a15c70e8f8a5a3fabe0ce8cfee02490214a2819b
geepy technologies <geepytechnologies@gmail.com>
2023-01-21 21:05:10 +0100

    new

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cors({origin: "https://gemukdiamond.com", credentials: true}));
 app.use((req,res,next)=>{
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", "https://gemukdiamond.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next();
