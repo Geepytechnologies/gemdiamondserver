978991a37283543462d0d172db2de47f733675d5
geepy technologies <geepytechnologies@gmail.com>
2023-01-22 11:17:34 +0100

    fresh

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors({origin: 'https://gemukdiamond.com', credentials:true}));
 app.use((req,res,next)=>{
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", 'https://gemukdiamond.com');
+  res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next();
 })
